e170189ebc3808c80c71fc31628ca35611d59973
Rafi Siddique <rafisiddique67@gmail.com>
2021-07-19 16:44:03 +0600

    A_Petya_and_Strings problem solved

---
+++ A_Petya_and_Strings.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s1, s2;
+    cin >> s1 >> s2;
+    for_each(s1.begin(), s1.end(), [](char &c) {
+        c = ::tolower(c);
+    });
+
+    for_each(s2.begin(), s2.end(), [](char &c) {
+        c = ::tolower(c);
+    });
+
+    if(s1==s2) cout << 0 << "\n";
+    else if(s1>s2) cout << 1 << "\n";
+    else cout << -1 << "\n";
+
+    return 0;
+
+}
