73ea4c3a1d9edbfea46def6d0efd051260e3087a
Rafi Siddique <rafisiddique67@gmail.com>
2021-07-28 12:39:37 +0600

    solution 2 of helpfull math

---
+++ Helpfullmath.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void sort(int *a, int size) {
+    int temp, i=0, j=1, condition = size-1;
+    while(i<condition) {
+            if(a[i]>a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+            if(j!=condition) j++;
+            else {
+                i++;
+                j = i+1;
+            }
+    }
+}
+
+void task() {
+    string data, data2;
+    cin >> data;
+    int s = data.size();
+    if(s==1) cout << data << "\n";
+    else {
+        regex reg("(.?(\\d+).?)");
+        regex_replace(back_inserter(data2), data.begin(), data.end(), reg, "$2 ");
+        int size = data2.size()/2;
+        int a[size];
+        stringstream ss(data2);
+        for(int i=0; i<size; i++) {
+            int num;
+            ss >> num;
+            a[i] = num;
+        }
+        sort(a, size);
+        int s2 = size-1;
+        for(int i=0; i<size; i++) {
+            cout << a[i];
+            if(i!=s2) cout << "+";
+        }
+        cout << "\n";
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    task();
+
+    return 0;
+
+}
