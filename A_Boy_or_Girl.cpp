d15db6ffc9b6fc236b6fee592048e6bfe302cf23
Rafi Siddique <rafisiddique67@gmail.com>
2021-08-05 11:07:30 +0600

    solved

--- A_Boy_or_Girl.cpp
+++ A_Boy_or_Girl.cpp
@@ -5,22 +5,28 @@ using namespace std;
 int main() {
     
     string s;
-    int n = 0, size, tracker = 0;
+    int n = 0, size;
     cin >> s;
     size = s.size();
-    int a[size];
     for(int i=0; i<size; i++) {
         for(int j=i+1; j<size; j++) {
             if(s[i]==s[j]) {
-                a[tracker] = j;
-                tracker++;
+                s[j] = '0';
             }
         }
     }
 
     for(int i=0; i<size; i++) {
-        
+        if(s[i]=='0') {
+            continue;
+        }
+        else {
+            n++;
+        }
     }
+
+    if((n%2)==0) printf("CHAT WITH HER!\n");
+    else printf("IGNORE HIM!\n");
 
     return 0;
 
