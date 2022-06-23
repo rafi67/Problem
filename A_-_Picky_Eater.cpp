7c5cea1edab16fc04878b6fdc3e8e9ec891af776
Rafi Siddique <rafisiddique67@gmail.com>
2022-06-23 09:04:00 +0600

    problem solved

---
+++ A_-_Picky_Eater.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x, y;
+
+    cin >> x >> y;
+
+    if(x==y || x>y) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    return 0;
+
+}
