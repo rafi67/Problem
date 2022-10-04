2d4276050d0d76bc12f950e84699b133ac4d3854
Rafi Siddique <rafisiddique67@gmail.com>
2022-10-04 20:20:48 +0600

    array replacement

---
+++ Array_Replacement_I.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int x[10];
+
+    for(int i=0; i<10; ++i) cin >> x[i];
+
+    for(int i=0; i<10; ++i) 
+        if(x[i]<=0) x[i] = 1;
+
+    for(int i=0; i<10; ++i)
+        cout << "X[" << i << "] = " << x[i] << "\n";
+
+    return 0;
+
+}
