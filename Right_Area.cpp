c5f51505e76107fb87f869fb4ddde79999add431
Rafi Siddique <rafisiddique67@gmail.com>
2022-10-05 23:26:21 +0600

    right area of diagonal

---
+++ Right_Area.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    float M[12][12], sum = 0;
+    int cnt = 0, k = 10;
+    char op;
+    cin >> op;
+
+    for(int i=0; i<12; ++i)
+        for(int j=0; j<12; ++j)
+            cin >> M[i][j];
+    
+    for(int i=1; i<6; ++i) {
+        for(int j=11; j>k; --j) {
+            sum += M[i][j];
+            ++cnt;
+        }
+        --k;
+    }
+
+    ++k;
+
+    for(int i=6; i<11; ++i) {
+        for(int j=11; j>k; --j) {
+            sum += M[i][j];
+            ++cnt;
+        }
+        ++k;
+    }
+
+    cout << fixed << setprecision(1);
+
+    if(op=='S')
+        cout << sum << "\n";
+    else
+        cout << sum/cnt << "\n";
+
+    return 0;
+
+}
