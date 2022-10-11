c62b2294f0525ff245fdd82d980dbf8dca7f18bb
Rafi Siddique <rafisiddique67@gmail.com>
2022-10-11 22:57:13 +0600

    printing sub string

---
+++ Our_Days_Are_Never_Coming_Back.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    string st = "LIFE IS NOT A PROBLEM TO BE SOLVED, BUT A REALITY TO BE EXPERIENCED.";
+    cout << st.substr(0, n) << "\n";
+
+    return 0;
+
+}
