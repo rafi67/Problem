dc30cc34292becf40e114475ef938320f6928dd9
Rafi Siddique <rafisiddique67@gmail.com>
2022-10-13 11:04:57 +0600

    helping to get the right decision

---
+++ Vitoria_and_Her_Indecision.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    string c;
+
+    cin >> t;
+
+    while(t--) 
+        getline(cin, c);
+
+    cout << "Ciencia da Computacao\n";
+
+    return 0;
+
+}
