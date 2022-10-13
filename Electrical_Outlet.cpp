dd40b5e75937e42d326bf179d931313dce8451f1
Rafi Siddique <rafisiddique67@gmail.com>
2022-10-13 13:06:12 +0600

    calculating electrical outlet

---
+++ Electrical_Outlet.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n1, n2, n3, n4;
+
+    cin >> n1 >> n2 >> n3 >> n4;
+
+    int sum = (n1-1)+(n2-1)+(n3-1)+n4;
+
+    cout << sum << "\n";
+
+    return 0;
+
+}
