a09608b848de4ee4b2892da053ec98bf33dd698b
Rafi Siddique <rafisiddique67@gmail.com>
2022-10-15 00:28:52 +0600

    Scientific notation

---
+++ Scientific_Notation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    double num;
+    cin >> num;
+
+    cout << showpos << fixed << setprecision(4) << scientific << uppercase 
+    << num << "\n";
+
+    return 0;
+
+}
