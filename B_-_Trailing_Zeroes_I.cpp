d54602543706473fa2a49b1104336663a32ed885
Rafi Siddique <rafisiddique67@gmail.com>
2022-11-19 21:24:43 +0600

    solved

--- B_-_Trailing_Zeroes_I.cpp
+++ B_-_Trailing_Zeroes_I.cpp
@@ -1,44 +1,48 @@
 #include <bits/stdc++.h>
-#define mx 1000105
 #define ll long long
+#define mx 1000000 // 10^6
 
 using namespace std;
 
 bool cross[mx+5];
-vector<int>prime;
+vector<ll>prime;
+int id = 1;
 
 void sieve() {
     for(int i=2; i<=mx; ++i) {
         if(cross[i]) continue;
         prime.push_back(i);
-        for(ll j = (ll)i*i; j<=mx; j+=i) cross[i] = true;
+        for(ll j = (ll)i*i; j<=mx; j+=i) cross[j] = true;
     }
 }
 
 int main() {
 
     sieve();
-    int t, j=1;
+
+    int t, cnt = 0, j = 1;
+    ll n;
     scanf("%d", &t);
+
     while(t--) {
-        ll n;
-        scanf("%lld", &n);
         int total_divisor = 1;
-        for(int p:prime) {
-            if(n<(ll)p*p) break;
+        scanf("%lld", &n);
+        for(ll p:prime) {
+            if(n<p*p) break;
             if(n%p==0) {
-                int count = 0;
+                cnt = 0;
                 while(n%p==0) {
+                    ++cnt;
                     n/=p;
-                    count++;
+                    if(n==0 || n==1) break;
                 }
-                total_divisor *= (count+1);
+                total_divisor *= cnt+1;
             }
         }
-        if(n>1) total_divisor*=2;
+        if(n>1) total_divisor *= 2;
         printf("Case %d: %d\n", j++, total_divisor-1);
     }
-    
+
     return 0;
-    
+
 }
